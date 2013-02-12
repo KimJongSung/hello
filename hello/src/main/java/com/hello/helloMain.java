354bd57e31754e33f548b6c482142dbb85622961
KimJongSung <shutgun21@gmail.com>
2013-02-12 23:07:48 +0900

    init

---
+++ hello/src/main/java/com/hello/helloMain.java
@@ -0,0 +1,13 @@
+package com.hello;
+
+public class helloMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+	}
+
+}
